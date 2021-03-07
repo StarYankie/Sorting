f556166bcf4aa38988a933f36c0057399243e068
zsd6th <junghancho@gmail.com>
2021-03-07 17:08:26 +0900

    Print 메서드 생성

--- QuickSort.java
+++ QuickSort.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 public class QuickSort {
-    public static void quickSort(int[] arr) {
+	private static void quickSort(int[] arr) {
     	quickSort(arr, 0, arr.length-1);
     }
-    public static void quickSort(int[] arr, int start, int end) {
+    private static void quickSort(int[] arr, int start, int end) {
     	if(arr.length == 1) return;
     	int partition = part(arr,start,end);
     	if(start < partition)
@@ -12,7 +12,7 @@ public class QuickSort {
     	if(partition < end)
     		quickSort(arr,0,partition-1);
     }
-    public static int part(int[] arr, int start, int end) {
+    private static int part(int[] arr, int start, int end) {
     	int pivot = arr[(start + end)/2];
     	while(start <= end) {
     		while(arr[start] < pivot) start++;
@@ -25,17 +25,24 @@ public class QuickSort {
     	}
     	return start;
     }
-    public static void swap(int[] arr, int i, int j) {
+    private static void swap(int[] arr, int i, int j) {
     	int temp = arr[i];
     	arr[i] = arr[j];
     	arr[j] = temp;
     }
+    public static void print(int[] arr) {
+    	for(int i:arr) {
+    		System.out.print(i + ", ");
+    	}
+    
+    	System.out.println();
+    }
 
     public static void main(String[] args) {
     	int[] input = {3,9,4,7,5,0,1,6,8,2};
+    	print(input);
+    	
     	quickSort(input);
-    	for(int i:input) {
-    		System.out.println(input[i]);
-    	}
+    	print(input);
     }
 }
